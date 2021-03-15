ebf5c9b3076f72d1360d9cf52a7e4232c919b862
sem-the-dev <semhar.tesfu@outlook.com>
2021-03-15 18:56:38 +0000

    add procfile

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
 const server = require('./server')
 
-server.listen(3000, () => console.log(`\n Express departing now from port 3000!\n`))
+const port = process.env.PORT || 3000
+server.listen(port, () => console.log(`\n Express departing now from port ${port}!\n`))
